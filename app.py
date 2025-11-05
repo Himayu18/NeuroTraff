643e1b08b793d5e24fa295b164ee4a8a7a26d507
Himayu18 <himayudhoke18@gmail.com>
2025-11-05 22:20:22 +0530

    remove binding port

--- app.py
+++ app.py
@@ -132,5 +132,5 @@ def selected_road():
 # ----------------- Run app -----------------
 if __name__ == '__main__':
     print("Starting Flask app...")
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
