e4f938afd25a602cd52ddc41aa9dd46a5b5dedbf
Himayu18 <himayudhoke18@gmail.com>
2025-11-06 09:21:52 +0530

    feedback issue

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def get_feedbackformdata():
         import Features.feedbackform as feedbackform
 
         feedbackform.get_response(data)
-        return jsonify({'status': 'success', 'message': 'Feedback received'})
+        return 'data feteched successfull'
     except Exception as e:
         from src.exception import CustomException
         raise CustomException(sys, e)
